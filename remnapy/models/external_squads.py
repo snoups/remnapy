1092fe9940579151ddabb66849a679e48e83246d
Ilay <isnoups@gmail.com>
2025-12-15 16:18:37 +0500

    fix: add default value for subscriptionSettings

--- remnapy/models/external_squads.py
+++ remnapy/models/external_squads.py
@@ -35,19 +35,21 @@ class ExternalSquadTemplateDto(BaseModel):
 class ExternalSquadSubscriptionSettingsDto(BaseModel):
     """External squad subscription settings"""
 
-    profile_title: Optional[str] = Field(alias="profileTitle")
-    support_link: Optional[str] = Field(alias="supportLink")
-    profile_update_interval: Optional[int] = Field(alias="profileUpdateInterval", ge=1)
+    profile_title: Optional[str] = Field(None, alias="profileTitle")
+    support_link: Optional[str] = Field(None, alias="supportLink")
+    profile_update_interval: Optional[int] = Field(
+        None, alias="profileUpdateInterval", ge=1
+    )
     is_profile_webpage_url_enabled: Optional[bool] = Field(
-        alias="isProfileWebpageUrlEnabled"
+        None, alias="isProfileWebpageUrlEnabled"
     )
     serve_json_at_base_subscription: Optional[bool] = Field(
-        alias="serveJsonAtBaseSubscription"
+        None, alias="serveJsonAtBaseSubscription"
     )
-    is_show_custom_remarks: Optional[bool] = Field(alias="isShowCustomRemarks")
+    is_show_custom_remarks: Optional[bool] = Field(None, alias="isShowCustomRemarks")
     happ_announce: Optional[str] = Field(None, alias="happAnnounce")
     happ_routing: Optional[str] = Field(None, alias="happRouting")
-    randomize_hosts: Optional[bool] = Field(alias="randomizeHosts")
+    randomize_hosts: Optional[bool] = Field(None, alias="randomizeHosts")
 
 
 class ExternalSquadHostOverridesDto(BaseModel):
